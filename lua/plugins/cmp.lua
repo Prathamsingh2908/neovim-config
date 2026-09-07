5707a0ea862fb71128af33f02006653b5f463565
Pratham Singh <ps3261712@gmail.com>
2026-09-07 18:00:29 +0530

    Update Neovim configuration

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,9 +1,6 @@
 local cmp = require("cmp")
 
 cmp.setup({
-    completion = {
-        autocomplete = false,
-    },
     mapping = cmp.mapping.preset.insert({
         ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-p>"] = cmp.mapping.select_prev_item(),
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,3 +8,15 @@ vim.lsp.config("clangd", {
 })
 
 vim.lsp.enable("clangd")
+
+vim.lsp.config("pyright", {
+    capabilities = capabilities,
+})
+
+vim.lsp.enable("pyright")
+
+vim.lsp.config("ts_ls", {
+    capabilities = capabilities,
+})
+
+vim.lsp.enable("ts_ls")
